9c1f4c6aa6bd4b30367609acffd3801dbbf99a0b
YazanRihan <yazanrihan@gmail.com>
2024-02-01 13:24:09 +0400

    Fixing usb ports

--- camco_launch/launch/camco.launch.py
+++ camco_launch/launch/camco.launch.py
@@ -1,19 +1,23 @@
 from launch import LaunchDescription
-from launch.actions import IncludeLaunchDescription, GroupAction
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
 from launch.substitutions import PathJoinSubstitution
-from launch_ros.actions import SetRemap, Node
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
+import yaml
 
 
 def generate_launch_description():
     ld = LaunchDescription()
 
-    # Get the path to camco_launch/rviz/camco.rviz
+    # Get the path to camco_launch/rviz/camco.rviz and kobuki params
     pkg_camco_launch = get_package_share_directory('camco_launch')
     
     camco_launch_rviz_config_file = PathJoinSubstitution(
         [pkg_camco_launch, 'rviz', 'camco.rviz'])
+    
+    kobuki_node_config_file = PathJoinSubstitution(
+        [pkg_camco_launch, 'config', 'kobuki_node_params.yaml'])
 
     # Get the path to camco_description.launch.py
     pkg_camco_description = get_package_share_directory('camco_description')
@@ -34,15 +38,18 @@ def generate_launch_description():
         launch_arguments={'rviz_config_file': camco_launch_rviz_config_file}.items()
         )
     
-    # Include kobuki_node-launch.py and remap /commands/velocity to /cmd_vel
-    kobuki_node_launch_include = GroupAction(
-        actions=[
-            SetRemap(src='commands/velocity',dst='cmd_vel'),
-            IncludeLaunchDescription(
-                PythonLaunchDescriptionSource([kobuki_node_launch_file])
-            )
-        ]
-    )
+    # Launch kobuki node and remap /commands/velocity to /cmd_vel
+
+    with open(kobuki_node_config_file, 'r') as f:
+        kobuki_params = yaml.safe_load(f)['kobuki_ros_node']['ros__parameters']
+    
+    kobuki_ros_node = Node(
+        package='kobuki_node',
+        node_executable='kobuki_ros_node',
+        output='both',
+        parameters=[kobuki_params],
+        remappings=[('commands/velocity', 'cmd_vel')]
+        )
 
     #Launching RPLIDAR node
     rplidar_node = Node(
@@ -52,7 +59,7 @@ def generate_launch_description():
             output='screen',
             parameters=[{
                 'serial_port': '/dev/RPLIDAR',
-                'serial_baudrate': 57600,
+                'serial_baudrate': 115200,
                 'frame_id': 'rplidar_link',
                 'inverted': False,
                 'angle_compensate': True,
@@ -61,7 +68,7 @@ def generate_launch_description():
         )
 
     ld.add_action(camco_description_launch_include)
-    ld.add_action(kobuki_node_launch_include)
+    ld.add_action(kobuki_ros_node)
     ld.add_action(rplidar_node)
 
     return ld
