a87f333742b0752ebec0c8c4468f7d8a0ce267fa
MBMYA <mustafa.alaraj@hotmail.com>
2024-02-03 00:11:44 +0400

    Add slam config and launch files

--- camco_navigation/launch/slam.launch.py
+++ camco_navigation/launch/slam.launch.py
@@ -12,10 +12,59 @@ arguments = [
 ]
 
 def generate_launch_description():
+    # Get path to camco_navigation/config/slam.yaml
     pkg_camco_navigation = get_package_share_directory('camco_navigation')
+    slam_node_config_file = PathJoinSubstitution([pkg_camco_navigation,'config', 'slam.yaml'])
     
+    # Launch configuration variables specific to SLAM
     sync = LaunchConfiguration('sync')
+
+    # Declare the launch arguments
     slam_params_arguments = DeclareLaunchArgument('params',
-        default_value=PathJoinSubstitution([pkg_camco_navigation,'config', 'slam.yaml']), description='Robot namespace'
+        default_value= slam_node_config_file, description='Robot namespace'
     )
-    
+
+    # Rewrite yaml configuration file for the SLAM
+    slam_params = RewrittenYaml(
+        source_file=LaunchConfiguration('params'),
+        root_key='',
+        param_rewrites={},
+        convert_types=True
+    )
+
+    # Remap topics' names
+    remappings = [
+        ('/tf', 'tf'),
+        ('/tf_static', 'tf_static'),
+        ('/scan', 'scan'),
+        ('/map', 'map'),
+        ('/map_metadata', 'map_metadata'),
+    ]
+
+    # Create a SLAM group action
+    slam = GroupAction([
+        # PushRosNamespace(namespace), # Test without namespacing these nodes to check if it is not needed
+
+        # Create a synchronized SLAM node
+        Node(package='slam_toolbox',
+             executable='sync_slam_toolbox_node',
+             name='slam_toolbox',
+             output='screen',
+             parameters=[slam_params],
+             remappings=remappings,
+             condition=IfCondition(sync)),
+        # Create an asyncronized SLAM node
+        Node(package='slam_toolbox',
+             executable='async_slam_toolbox_node',
+             name='slam_toolbox',
+             output='screen',
+             parameters=[slam_params],
+             remappings=remappings,
+             condition=UnlessCondition(sync))
+    ])
+
+    # Declare launch options and add action
+    ld = LaunchDescription(arguments)
+    ld.add_action(slam_params_arguments)
+    ld.add_action(slam)
+    return ld
