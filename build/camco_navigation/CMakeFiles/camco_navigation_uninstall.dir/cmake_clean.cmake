8a9ba775a10f9f82bb25dacbff3f3ac2ee27c120
YazanRihan <yazanrihan@gmail.com>
2024-02-17 23:07:19 +0400

    Removed build files

--- build/camco_navigation/CMakeFiles/camco_navigation_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/camco_navigation_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/camco_navigation_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
