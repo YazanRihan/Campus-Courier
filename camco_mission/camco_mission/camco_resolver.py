467922cf318eb574157a234d5df90e5e18d48c5f
YazanRihan <yazanrihan@gmail.com>
2024-04-04 14:42:27 +0400

    Finalized camco_resolver node

--- camco_mission/camco_mission/camco_resolver.py
+++ camco_mission/camco_mission/camco_resolver.py
@@ -1,88 +1,68 @@
-#! /usr/bin/env python3
-# Copyright 2021 Samsung Research America
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-
-from geometry_msgs.msg import PoseStamped
-from std_msgs.msg import String
-from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
-import rclpy
-from rclpy.duration import Duration
-import yaml
 import os
+import yaml
+import rclpy
 from rclpy.node import Node
+from rclpy.duration import Duration
+from camco_msgs.msg import RoomAddress
+from geometry_msgs.msg import PoseStamped
+from nav2_simple_commander.robot_navigator import BasicNavigator, TaskResult
 from ament_index_python.packages import get_package_share_directory
 
-"""
-Basic navigation demo to go to pose.
-"""
 class CamcoResolver(Node):
 
     def __init__(self):
         super().__init__('camco_resolver')
 
-        self.initial_subscription = self.create_subscription(String,'initial_room',self.initial_listener_callback,10)
+        self.initial_subscription = self.create_subscription(RoomAddress,'initial_address',self.initial_listener_callback,10)
         self.initial_subscription  # prevent unused variable warning
 
-        self.goal_subscription = self.create_subscription(String,'goal_room',self.goal_listener_callback,10)
+        self.goal_subscription = self.create_subscription(RoomAddress,'goal_address',self.goal_listener_callback,10)
         self.goal_subscription  # prevent unused variable warning
 
         self.navigator = BasicNavigator()
 
         pkg_camco_mission = get_package_share_directory('camco_mission')
-        self.rooms_yaml_path = os.path.join(pkg_camco_mission, 'room_numbers.yaml')
+        self.addr_book_yaml_path = os.path.join(pkg_camco_mission, 'address_book.yaml')
 
     def initial_listener_callback(self, msg):
-        self.get_logger().info('Initial Room Callback: "%s"' % msg.data)
+        self.get_logger().info(f'Initial Address Callback: Building {msg.building} - Room {msg.room}')
         
-        initial_room = msg.data
-        initial_building = initial_building = initial_room.split('-')[0]
+        initial_building = msg.building
+        initial_room = msg.room
 
-        with open(self.rooms_yaml_path, 'r') as roomsResolver:
-            rooms = yaml.full_load(roomsResolver)
-
+        with open(self.addr_book_yaml_path, 'r') as f:
+            addr_book = yaml.full_load(f)
+        
         initial_pose = PoseStamped()
         initial_pose.header.frame_id = 'map'
         initial_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        initial_pose.pose.position.x = float(rooms.get(initial_building).get(initial_room).get("x"))
-        initial_pose.pose.position.y = float(rooms.get(initial_building).get(initial_room).get("y"))
-        initial_pose.pose.orientation.z = float(rooms.get(initial_building).get(initial_room).get("z"))
-        initial_pose.pose.orientation.w = float(rooms.get(initial_building).get(initial_room).get("w"))
-        
-        print("Resolved initial pose is: ", initial_pose.__repr__())
+        initial_pose.pose.position.x = float(addr_book.get(initial_building).get(initial_room).get("x"))
+        initial_pose.pose.position.y = float(addr_book.get(initial_building).get(initial_room).get("y"))
+        initial_pose.pose.orientation.z = float(addr_book.get(initial_building).get(initial_room).get("z"))
+        initial_pose.pose.orientation.w = float(addr_book.get(initial_building).get(initial_room).get("w"))
+        self.get_logger().info(f"Resolved initial pose is: {initial_pose.__repr__()}")
 
         self.navigator.setInitialPose(initial_pose)
     
     def goal_listener_callback(self, msg):
-        self.get_logger().info('Goal Room Callback: "%s"' % msg.data)
+        self.get_logger().info(f'Initial Address Callback: Building {msg.building} - Room {msg.room}')
         
-        goal_room = msg.data
-        goal_building = goal_room.split('-')[0]
+        goal_building = msg.building
+        goal_room = msg.room
 
-        with open(self.rooms_yaml_path, 'r') as roomsResolver:
-            rooms = yaml.full_load(roomsResolver)
+        with open(self.addr_book_yaml_path, 'r') as f:
+            addr_book = yaml.full_load(f)
         
         self.navigator.waitUntilNav2Active()
 
         goal_pose = PoseStamped()
         goal_pose.header.frame_id = 'map'
         goal_pose.header.stamp = self.navigator.get_clock().now().to_msg()
-        goal_pose.pose.position.x = float(rooms.get(goal_building).get(goal_room).get("x"))
-        goal_pose.pose.position.y = float(rooms.get(goal_building).get(goal_room).get("y"))
-        goal_pose.pose.orientation.z = float(rooms.get(goal_building).get(goal_room).get("z"))
-        goal_pose.pose.orientation.w = float(rooms.get(goal_building).get(goal_room).get("w"))
-
-        print("Resolved goal pose is: ", goal_pose.__repr__())
+        goal_pose.pose.position.x = float(addr_book.get(goal_building).get(goal_room).get("x"))
+        goal_pose.pose.position.y = float(addr_book.get(goal_building).get(goal_room).get("y"))
+        goal_pose.pose.orientation.z = float(addr_book.get(goal_building).get(goal_room).get("z"))
+        goal_pose.pose.orientation.w = float(addr_book.get(goal_building).get(goal_room).get("w"))
+        self.get_logger().info(f"Resolved goal pose is: {goal_pose.__repr__()}")
 
         self.navigator.goToPose(goal_pose)
 
--- camco_mission/setup.py
+++ camco_mission/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages',
             ['resource/' + package_name]),
-        ('share/' + package_name, ['resource/lookup/room_numbers.yaml']),
+        ('share/' + package_name, ['resource/lookup/address_book.yaml']),
         ('share/' + package_name, ['package.xml']),
     ],
     install_requires=['setuptools'],
