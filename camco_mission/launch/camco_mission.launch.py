3056f4fea0c79aea7b4967a002e7f753d6adb217
YazanRihan <yazanrihan@gmail.com>
2024-05-05 20:52:47 +0400

    Added robot_pose_publisher and rosbridge_websocket to launch file

--- camco_mission/launch/camco_mission.launch.py
+++ camco_mission/launch/camco_mission.launch.py
@@ -4,9 +4,11 @@ from launch.actions import (
     IncludeLaunchDescription,
 )
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_xml.launch_description_sources import XMLLaunchDescriptionSource
 from launch.substitutions import LaunchConfiguration, PathJoinSubstitution
 from ament_index_python.packages import get_package_share_directory
 from launch_ros.actions import Node
+from launch.conditions import IfCondition
 
 ARGUMENTS = [
     DeclareLaunchArgument('params_nav2',
@@ -24,22 +26,29 @@ ARGUMENTS = [
             description='Localization parameters'),
     DeclareLaunchArgument('map', default_value='w12.yaml',
         description='name of the map yaml file to load'),
-     DeclareLaunchArgument('rviz', default_value='true',
-        description='Whether to use Rviz2 or not')
+    DeclareLaunchArgument('rviz', default_value='true',
+        description='Whether to use Rviz2 or not'),
+    DeclareLaunchArgument('gui', default_value='true',
+        description='Whether to use GUI or not')
 ]
 
 
 def generate_launch_description():
 
     pkg_camco_navigation = get_package_share_directory('camco_navigation')
+    pkg_rosbridge_server = get_package_share_directory('rosbridge_server')
+    pkg_robot_pose_publisher_ros2 = get_package_share_directory('robot_pose_publisher_ros2')
 
     path_camco_navigation_launch = PathJoinSubstitution([pkg_camco_navigation, 'launch', 'navigation.launch.py'])
-    
+    path_rosbridge_websocket_launch = PathJoinSubstitution([pkg_rosbridge_server, 'launch', 'rosbridge_websocket_launch.xml'])
+    path_robot_pose_publisher_launch = PathJoinSubstitution([pkg_robot_pose_publisher_ros2, 'launch', 'robot_pose_publisher_launch.py'])
+
     namespace = LaunchConfiguration('namespace')
     use_sim_time = LaunchConfiguration('use_sim_time')
     params_nav2 = LaunchConfiguration('params_nav2')
     params_amcl = LaunchConfiguration('params_amcl')
     rviz = LaunchConfiguration('rviz')
+    gui = LaunchConfiguration('gui')
 
 
     camco_navigation_launch = IncludeLaunchDescription(
@@ -63,9 +72,20 @@ def generate_launch_description():
     executable='camco_mission',
     output='screen')
 
+    robot_pose_publisher_launch = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(path_robot_pose_publisher_launch)
+    )
+
+    rosbridge_websocket_launch = IncludeLaunchDescription(
+        XMLLaunchDescriptionSource(path_rosbridge_websocket_launch),
+        condition=IfCondition(gui)
+    )
+
     
     ld = LaunchDescription(ARGUMENTS)
     ld.add_action(camco_navigation_launch)
     ld.add_action(read_battery_state_server_node)
     ld.add_action(camco_mission_node)
+    ld.add_action(robot_pose_publisher_launch)
+    ld.add_action(rosbridge_websocket_launch)
     return ld
